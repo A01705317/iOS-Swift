da3ba0ff48c2beee9a34c0346de54bda7adba0b9
A01705317 <47190742+A01705317@users.noreply.github.com>
2021-06-01 13:32:35 -0500

    Movie List App Finished

---
+++ MovieListApp/MovieList/MovieListApp.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Miguel Angel Marines Olvera on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            ContentView()
+        }
+    }
+}
